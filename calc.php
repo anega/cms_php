f3b9234bbaed4a0288e388cffe5352e94b63b775
anega <anegamega@gmail.com>
2019-01-12 15:26:12 +0200

    Add trigonomentric math functions.

--- calc.php
+++ calc.php
@@ -34,3 +34,28 @@ function logarithm($a) {
 function exponent($a) {
   return exp($a);
 }
+
+// Trigonometric math
+function sine($a) {
+  return sin($a);
+}
+
+function aSine($a) {
+  return asin($a);
+}
+
+function cosine($a) {
+  return cos($a);
+}
+
+function aCosine($a) {
+  return acos($a);
+}
+
+function tangent($a) {
+  return tan($a);
+}
+
+function aTangent($a) {
+  return atan($a);
+}
