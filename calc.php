2755d09d5e346357a339641688bf9a25632028fc
anega <anegamega@gmail.com>
2019-01-12 15:25:08 +0200

    Add simple math functions.

--- calc.php
+++ calc.php
@@ -1,2 +1,19 @@
 <?php
 
+// Simple math
+function plus($a, $b) {
+  return $a + $b;
+}
+
+function minus($a, $b) {
+  return $a - $b;
+}
+
+function multiply($a, $b) {
+  return $a * $b;
+}
+
+function devide($a, $b) {
+  if ($b === 0) return "Can't devide by 0.";
+  return $a / $b;
+}
