9e8d39871337baf3b2fd9f1398e4ae0a7df7e64f
anega <anegamega@gmail.com>
2019-01-12 15:25:36 +0200

    Add math functions.

--- calc.php
+++ calc.php
@@ -17,3 +17,20 @@ function devide($a, $b) {
   if ($b === 0) return "Can't devide by 0.";
   return $a / $b;
 }
+
+// Math functions
+function powerOf($a, $b) {
+  return pow($a, $b);
+}
+
+function root($a, $b) {
+  return pow($a, 1 / $b);
+}
+
+function logarithm($a) {
+  return log($a);
+}
+
+function exponent($a) {
+  return exp($a);
+}
